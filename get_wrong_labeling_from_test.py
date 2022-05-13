50cae9b5c00a59ab934b236b307dfb747d55ab6e
danielc <s8hrjK3D9gzRksxayoLK>
2022-05-13 22:46:42 +0200

    add script for getting wrong labels

--- get_wrong_labeling_from_test.py
+++ get_wrong_labeling_from_test.py
@@ -39,12 +39,29 @@ for file in file_list_2:
                     except KeyError:
                         false_labels_2[f'{values[0]} {values[1]}'] = 1
 
-
+m1_better = 0
+m2_better = 0
+same = 0
+only_m1 = 0
 for wrong_label in false_labels_1.keys():
     try:
         if false_labels_1[wrong_label] > false_labels_2[wrong_label]:
-            print(f'{wrong_label}: m1-({false_labels_1[wrong_label]}) m2-({false_labels_2[wrong_label]}) #############')
+            m2_better += 1
+            print(f'{wrong_label}: m1-({false_labels_1[wrong_label]} times wrong) '
+                  f':: m2-({false_labels_2[wrong_label]} times wrong) ############# m2 BETTER!')
+        elif false_labels_1[wrong_label] < false_labels_2[wrong_label]:
+            m1_better += 1
+            print(f'{wrong_label}: m1-({false_labels_1[wrong_label]} times wrong) '
+                  f':: m2-({false_labels_2[wrong_label]} times wrong) ############# m1 BETTER!')
         else:
-            print(f'{wrong_label}: m1-({false_labels_1[wrong_label]}) m2-({false_labels_2[wrong_label]})')
+            same += 1
+            print(f'{wrong_label}: m1-({false_labels_1[wrong_label]} times wrong) '
+                  f':: m2-({false_labels_2[wrong_label]} times wrong)')
     except KeyError:
+        only_m1 += 1
         print(f'{wrong_label}: m1-({false_labels_1[wrong_label]})')
+
+print(f'm1 better {m1_better} times')
+print(f'm2 better {m2_better} times')
+print(f'same error by both {same} times')
+print(f'only m1 error {only_m1} times')
